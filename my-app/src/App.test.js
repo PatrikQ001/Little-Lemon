3c91b1e1e0b79e82301630f12db327140f811eff
PatrikQ001 <pqn04@hotmail.com>
2025-01-20 02:19:40 -0500

    Unit test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingForm from './components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('form renders correctly ', () => {
+    render(<BookingForm availableTimes={{ availableTimes: ['17:00 PM', '18:00 PM'] }} dispatch={() => {}} />);
+    expect(screen.getByLabelText(/Choose Date/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Choose Time/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Number of Guests/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Occasion/)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Make Your Reservation/ })).toBeInTheDocument();
 });
